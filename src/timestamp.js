57cb5a302f32c470db5d35b8e7b56e7267198baa
Samuel Skånberg <samuel.skanberg@nexusgroup.com>
2015-09-18 12:12:39 +0200

    Added timestamps

--- src/timestamp.js
+++ src/timestamp.js
@@ -1,6 +1,6 @@
 var data = [
-  {date: "2015-07-01", hours: 8},
-  {date: "2015-07-02", hours: 8}
+  {date: "2015-07-01", hours: 8, timestamps: [{start: '09:00', end: '12:00'}, {start: '13:00', end: '16:00'} ]},
+  {date: "2015-07-02", hours: 8, timestamps: [{start: '08:30', end: '12:00'}, {start: '13:00', end: '16:30'} ]}
 ];
 
 var DayBox = React.createClass({
@@ -13,7 +13,7 @@ var DayBox = React.createClass({
   render: function() {
     var dayNodes = this.props.data.map(function (day) {
       return (
-        <Day date={day.date} hours={day.hours}></Day>
+        <Day date={day.date} hours={day.hours} timestamps={day.timestamps}></Day>
       );
     });
     return (
@@ -26,14 +26,39 @@ var DayBox = React.createClass({
 });
 var Day = React.createClass({
   render: function() {
+    var timestampNodes = this.props.timestamps.map(function (timestamp) {
+      return (
+        <Timestamp start={timestamp.start} end={timestamp.end}></Timestamp>
+      );
+    });
     return (
       <div className="day">
         <h2 className="dayDate">
           {this.props.date}
         </h2>
+        <div className="dayTimestamps">
+          {timestampNodes}
+        </div>
         <h3 className="dayHours">
           {this.props.hours}
         </h3>
+      </div>
+    );
+  }
+});
+
+var Timestamp = React.createClass({
+  render: function() {
+    return (
+      <div className="timestamp">
+        <ul>
+          <li className="timestampStart">
+            {this.props.start}
+          </li>
+          <li className="timestampEnd">
+            {this.props.end}
+          </li>
+        </ul>
       </div>
     );
   }
